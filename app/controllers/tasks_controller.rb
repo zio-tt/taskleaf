0f13fe3976e076940df5cc5e31df33271bd4434c
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-05-28 17:12:06 +0900

    create_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,18 @@ class TasksController < ApplicationController
 
   def edit
   end
+
+  def create
+    # task_paramsメソッドで安全化されたtaskパラメータを取得する
+    task = Task.new(task_params)
+    # save!メソッドはsaveメソッドと違い失敗した時に例外を返す（saveはfalseを返す）
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
 end
