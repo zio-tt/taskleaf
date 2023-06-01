90d8b68c47aba9687a7501530517ff2645b875cc
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-06-01 23:21:20 +0900

    add valid_name_not_include_comma

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,12 @@
 class Task < ApplicationRecord
 	validates :name, presence: true
-	validate :name, length: { maximum: 30 }
+	validates :name, length: { maximum: 30 }
 	validate :validate_name_not_including_comma
 
 	private
 
 	def validate_name_not_including_comma
-		errors.add(:name, 'にカンマを含めることはできません') if name&.inlude?(',')
+		errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
 	end
 
 end
