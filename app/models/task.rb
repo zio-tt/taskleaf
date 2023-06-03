213dc690103dffae203e2b27586996ec409379fe
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-06-03 23:16:06 +0900

    fix typo_inheritance

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
-class Task < ApplicationRecoRD
+class Task < ApplicationRecord
 	validates :name, presence: true
 	validates :name, length: { maximum: 30 }
 	validate :validate_name_not_including_comma
