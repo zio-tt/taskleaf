4cc4b24f6f2e6a65e2d1fd9326a443684a53e4c6
terao_tomohiro_44 <zio.tt.dev@gmail.com>
2023-06-05 23:13:53 +0900

    add logger_daily_and_weekly

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.logger = Logger.new('log/development.log', 'daily')
+  config.custom_logger = Logger.new('log/custom.log', 'weekly')
 end
